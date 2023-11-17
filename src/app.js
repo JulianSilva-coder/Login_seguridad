f96f41d0e0151d896ce3f05c4960fcffd379220f
Juliansilva-coder <jsilvabe@unbosque.edu.co>
2023-11-16 23:20:52 -0500

    login

--- src/app.js
+++ src/app.js
@@ -1,36 +1,18 @@
 const express = require('express');
-const app = express();
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
 const { engine } = require('express-handlebars');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const loginRoutes = require('./routes/login');
 
-// Configuración de las opciones para HTTPS
-const options = {
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-  passphrase: 'julian'
-};
 
-// Middleware de redirección de HTTP a HTTPS
-const redirectHttp = (req, res, next) => {
-  if (req.protocol === 'http') {
-    res.redirect('https://' + req.headers.host + req.url);
-  } else {
-    next();
-  }
-};
+const app = express();
+app.set('port', 4000);
 
-// Configuración de Express
-app.set('port', 443);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -53,25 +35,12 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// Redirigir las solicitudes de HTTP al servidor HTTPS
-const httpServer = http.createServer((req, res) => {
-  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-  res.end();
+app.listen(app.get('port'), () => {
+  console.log('Listening on port ', app.get('port'));
 });
 
-httpServer.listen(80, () => {
-  console.log('HTTP server is listening on port 80');
-});
-
-// Rutas
-app.use(redirectHttp);
-app.use('/', loginRoutes);
+app.use('/',  loginRoutes);
 
 app.get('/', (req, res) => {
   res.render('home');
-});
-
-// Crear el servidor HTTPS
-https.createServer(options, app).listen(app.get('port'), () => {
-  console.log('Listening on port ', app.get('port'));
 });
